72192c43dcde59c60a6f53ff55b8d89a44ef852e
Labing I5 <Labing I5@LabingI5-18-101>
2018-07-25 11:20:08 -0500

    Commit inicial HolaMundo

---
+++ src/HolaMundo.java
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * @author Labing I5
+ * @since 25.07.2018
+ * 
+ */
+public class HolaMundo {
+    
+    /**
+     * 
+     * @param args
+     * 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    
+    }
+}
